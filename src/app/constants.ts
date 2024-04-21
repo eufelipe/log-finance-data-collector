406ee69b1534837441b4845b1af48025d4c62cec
Felipe Rosas <contato@eufelipe.com>
2024-04-21 14:46:51 -0300

    chore: add constants

---
+++ src/app/constants.ts
@@ -0,0 +1,9 @@
+export const ENCODING = "utf-8";
+
+export const BASE_CVM_URL = "https://dados.cvm.gov.br/dados/CIA_ABERTA/DOC";
+
+export const TEMP_DIR = "DATA/temp";
+
+export const CVM_DIR = "DATA/cvm";
+export const CVM_ORIGIN_RAW_DIR = "DATA/cvm/origin";
+export const CVM_MERGED_DIR = "DATA/cvm/merged";
